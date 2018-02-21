e632a8e42e73b350d0e24b84733e7e557571c0f3
Srikanth Grandhe <sgrandhe@cs.umass.edu>
2018-02-20 23:37:45 -0500

    Add extract stats to obtain language statistics

---
+++ preprocessing_tools/extract_stats.py
@@ -0,0 +1,37 @@
+'''
+This file contains code to extract the language metrics from the question json files
+'''
+import os
+import sys
+import json
+
+
+def main():
+    json_filepath = sys.argv[1]
+    json_data = file(json_filepath).read()
+    question_list = json.loads(json_data)
+    word_dictionary = {}
+    answer_dictionary = {}
+    for item in question_list:
+        #print item
+        question = item['question']
+        answer = item['answer']
+        word_list = question.split(' ')
+        for word in word_list:
+            if word == '':
+                continue
+            word_dictionary[word] = True
+        if answer == '':
+            continue
+        answer_dictionary[answer] = True
+    
+    print('Question vocab size = ' + str(len(word_dictionary.keys())))
+    print('Answer vocab size = ' + str(len(answer_dictionary.keys())))
+    print('question vocab =' + str(word_dictionary.keys()))
+    print('answer vocab = ' +  str(answer_dictionary.keys()))
+
+
+
+if __name__ == '__main__':
+    main()
+ 
