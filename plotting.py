dfb908a4f1e1e1dba0f6494e3d172ccd751c3e9d
Srikanth Grandhe <sgrandhe@cs.umass.edu>
2018-04-26 18:46:29 -0400

    Update plotting

--- plotting.py
+++ plotting.py
@@ -3,42 +3,80 @@ import sys
 import matplotlib.pyplot as plt
 
 
+SOURCE_FOLDER_PATH = '/home/srikanth/vqa/plotting_folder/'
+DESTINATION_FOLDER_PATH = '/home/srikanth/vqa/plotting_images/'
+def generate_loss_plots(plot_title, file_list, epoch_count):
+    plt.clf()
+    plt.title(plot_title)
+    plt.xlabel('Epochs')
+    plt.ylabel('Cross Entropy Loss')
+    #plt.xlim((0, 100))
+    #plt.ylim(0, 1.0)
+    for file_name in file_list:
+        temp_file = open(SOURCE_FOLDER_PATH + file_name)
+        train_loss_list = []
+        validation_loss_list = []
+        count = 0
+        for line in temp_file.readlines():
+            # extract all the train and validation loss values
+            word_list = line.replace('\n', '').split(' ')
+            if word_list[0] == 'Loss':
+                if count % 2 == 0:
+                    train_loss_list.append(float(word_list[2]))
+                else:
+                    validation_loss_list.append((float(word_list[2])))
+                count += 1
+
+        temp_file.close()
+        train_loss_list = train_loss_list[0 : epoch_count]
+        validation_loss_list = validation_loss_list[0 : epoch_count]
+        plt.plot(xrange(len(train_loss_list)), train_loss_list, label = 'Train loss')
+        plt.plot(xrange(len(validation_loss_list)), validation_loss_list, label = 'Validation loss')
+    
+    plt.legend(loc = 3)
+    #plt.show()
+    plt.savefig(DESTINATION_FOLDER_PATH+ plot_title + '_loss.png')
+
+def generate_accuracy_plots(plot_title, file_list, inital_value):
+    plt.clf()
+    plt.title(plot_title)
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    #plt.xlim((0, 100))
+    plt.ylim(0, 1.0)
+    for file_name in file_list:
+        temp_file = open(SOURCE_FOLDER_PATH + file_name)
+        train_accuracy_list = [inital_value]
+        validation_accuracy_list = [inital_value]
+        count = 0
+        for line in temp_file.readlines():
+            # extract all the train and validation loss values
+            word_list = line.replace('\n', '').split(' ')
+            if word_list[0] == 'Accuracy':
+                if count % 2 == 0:
+                    train_accuracy_list.append(float(word_list[2]))
+                else:
+                    validation_accuracy_list.append((float(word_list[2])))
+                count += 1
+
+        temp_file.close()
+        plt.plot(xrange(len(train_accuracy_list)), train_accuracy_list, label = 'Train accuracy')
+        plt.plot(xrange(len(validation_accuracy_list)), validation_accuracy_list, label = 'Validation accuracy')
+    
+    plt.legend(loc = 3)
+    #plt.show()
+    plt.savefig(DESTINATION_FOLDER_PATH + plot_title + '_accuracy.png' )
 
 
 def main():
-    title = 'Group Attention in RN'
-    plt.title(title)
-    plt.xlabel('iterations')
-    plt.ylabel('validation accuracy')
-    plt.ylim(0.0, 1.0)
-    y_list = [0.0, 0.57, 0.625, 0.5781, 0.5781, 0.6875, 0.7343, 0.7123, 0.764, 0.743, 0.722]
-    x_list = [i * 100 for i in xrange(len(y_list))]
-    plt.plot(x_list, y_list, label = 'Standard group attention')
-    y_list = [0.0, 0.55, 0.593, 0.5781, 0.5881, 0.6375, 0.6643, 0.6423, 0.694, 0.713, 0.722]
-    x_list = [i * 100 for i in xrange(len(y_list))]
-    plt.plot(x_list, y_list, label = 'Alternate group attention')
-    y_list = [0.0, 0.59, 0.605, 0.625, 0.632, 0.667, 0.6743, 0.719, 0.734, 0.755, 0.722]
-    x_list = [i * 100 for i in xrange(len(y_list))]
-    plt.plot(x_list, y_list, label = 'Group self-attention')
-    plt.legend(loc = 2)
-    
-    plt.show()
+    file_names = os.listdir(SOURCE_FOLDER_PATH)
+    for filename in file_names:
+        print('plotting for file: ' + filename)
+        plot_title = filename.replace('.txt', '')
+        file_list = [filename]
+        generate_accuracy_plots(plot_title + ' accuracy plot', file_list, 0.1)
+        generate_loss_plots(plot_title + ' loss plot', file_list, 50)
 
-    plt.title(title)
-    plt.xlabel('iterations')
-    plt.ylabel('training loss')
-    y_list = [0.684, 0.674, 0.6643, 0.6652, 0.6733, 0.6535, 0.6329, 0.623, 0.618, 0.593]
-    x_list = [i * 100 for i in xrange(len(y_list))]
-    plt.plot(x_list, y_list, label = 'Standard group attention')
-    y_list = [0.84, 0.74, 0.764, 0.715, 0.693, 0.6835, 0.6829, 0.673, 0.658, 0.642]
-    x_list = [i * 100 for i in xrange(len(y_list))]
-    plt.plot(x_list, y_list, label = 'Alternate group attention')
-    y_list = [0.78, 0.724, 0.6943, 0.6852, 0.6733, 0.6715, 0.6529, 0.643, 0.648, 0.637]
-    x_list = [i * 100 for i in xrange(len(y_list))]
-    plt.plot(x_list, y_list, label = 'Group self-attention')
-    plt.legend(loc = 2)
-    plt.show()
-    
 
 
 if __name__ == '__main__':
