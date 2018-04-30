Bug fix : Change dimension in softmax function

architectures/bn_modified_film.py
@@ -153,6 +153,6 @@ class bn_mod_FiLM(nn.Module):
             out, lin_h = resblock(out, i, lin_h)
 
         out = self.classifier(out)
-        out = F.log_softmax(out, dim=0)
+        out = F.log_softmax(out, dim=1)
 
         return out

architectures/film.py
@@ -137,6 +137,6 @@ class FiLM(nn.Module):
             out = resblock(out, film[i * 2], film[i * 2 + 1])
 
         out = self.classifier(out)
-        out = F.log_softmax(out, dim=0)
+        out = F.log_softmax(out, dim=1)
        
         return out

architectures/modified_film.py
@@ -150,6 +150,6 @@ class mod_FiLM(nn.Module):
             rnn_inp = res_lin(res_out)
 
         out = self.classifier(out)
-        out = F.log_softmax(out, dim=0)
+        out = F.log_softmax(out, dim=1)
 
         return out